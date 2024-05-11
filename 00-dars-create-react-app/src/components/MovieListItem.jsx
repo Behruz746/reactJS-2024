6cadb8ef589096e44a7c17160551f079988e306c
Behruz <madaminovbehruz746@gmail.com>
2024-05-11 21:55:27 +0500

    added: event in react

--- 00-dars-create-react-app/src/components/MovieListItem.jsx
+++ 00-dars-create-react-app/src/components/MovieListItem.jsx
@@ -1,28 +1,92 @@
 import React from "react";
 
-function MovieListItem({ name, views, favorite }) {
-  return (
-    <li
-      className={`list-group-item d-flex jusrify-content-between ${
-        favorite && "increase"
-      }`}
-    >
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="number"
-        className="list-group-item-input"
-        defaultValue={views}
-      />
-      <div className="d-flex justify-center align-item-center">
-        <button type="button" className="btn-cookie btn-sm">
-          <i className="fas fa-cookie"></i>
-        </button>
-        <button type="button" className="btn-trash btn-sm">
-          <i className="fas fa-trash"></i>
-        </button>
-      </div>
-    </li>
-  );
+class MovieListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: false,
+      like: false,
+    };
+  }
+
+  favoriteHandel = () => {
+    this.setState(({ favorite }) => ({
+      favorite: !favorite,
+    }));
+  };
+
+  likeHandel = () => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
+
+  deleteHandel = () => {};
+
+  render() {
+    const { name, views } = this.props;
+    const { favorite, like } = this.state;
+    return (
+      <li
+        className={`list-group-item d-flex jusrify-content-between ${
+          favorite && "increase"
+        } ${like && "like"}`}
+      >
+        <span className="list-group-item-label" onClick={this.likeHandel}>
+          {name}
+        </span>
+        <input
+          type="number"
+          className="list-group-item-input"
+          defaultValue={views}
+        />
+        <div className="d-flex justify-center align-item-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.favoriteHandel}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+          <button
+            type="button"
+            className="btn-trash btn-sm"
+            onClick={this.deleteHandel}
+          >
+            <i className="fas fa-trash"></i>
+          </button>
+
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
 }
+
+// function MovieListItem({ name, views, favorite }) {
+//   return (
+//     <li
+//       className={`list-group-item d-flex jusrify-content-between ${
+//         favorite && "increase"
+//       }`}
+//     >
+//       <span className="list-group-item-label">{name}</span>
+//       <input
+//         type="number"
+//         className="list-group-item-input"
+//         defaultValue={views}
+//       />
+//       <div className="d-flex justify-center align-item-center">
+//         <button type="button" className="btn-cookie btn-sm">
+//           <i className="fas fa-cookie"></i>
+//         </button>
+//         <button type="button" className="btn-trash btn-sm">
+//           <i className="fas fa-trash"></i>
+//         </button>
+//         <i className="fas fa-star"></i>
+//       </div>
+//     </li>
+//   );
+// }
 
 export default MovieListItem;
--- 00-dars-create-react-app/src/components/TestBlock.jsx
+++ 00-dars-create-react-app/src/components/TestBlock.jsx
@@ -35,6 +35,7 @@ class Count extends React.Component {
     super(props);
     this.state = {
       counter: 0,
+      age: "",
     };
   }
 
@@ -53,50 +54,67 @@ class Count extends React.Component {
     this.setState((prevState) => ({ counter: 0 }));
   };
 
+  changeHandler = (e) => {
+    this.setState((prevState) => ({ age: e.target.value }));
+  };
+
   render() {
     return (
-      <div style={{ width: "100%", display: "flex", gap: "10px" }}>
-        <button
-          style={{
-            padding: "10px 20px",
-            border: 0,
-            borderRadius: "5px",
-            backgroundColor: "#ccc",
-          }}
-          onClick={this.countPlus}
-        >
-          UP
-        </button>
-        <button
-          style={{
-            padding: "10px 20px",
-            border: 0,
-            borderRadius: "5px",
-            backgroundColor: "#ccc",
-          }}
-          onClick={this.countMinus}
-        >
-          DOWN
-        </button>
-        <button
-          style={{
-            padding: "10px 20px",
-            border: 0,
-            borderRadius: "5px",
-            backgroundColor: "#ccc",
-          }}
-          onClick={this.countReset}
-        >
-          RESET
-        </button>
-        <h1>{this.state.counter}</h1>
-      </div>
+      <>
+        <div style={{ width: "100%", display: "flex", gap: "10px" }}>
+          <button
+            style={{
+              padding: "10px 20px",
+              border: 0,
+              borderRadius: "5px",
+              backgroundColor: "#ccc",
+            }}
+            onClick={this.countPlus}
+          >
+            UP
+          </button>
+          <button
+            style={{
+              padding: "10px 20px",
+              border: 0,
+              borderRadius: "5px",
+              backgroundColor: "#ccc",
+            }}
+            onClick={this.countMinus}
+          >
+            DOWN
+          </button>
+          <button
+            style={{
+              padding: "10px 20px",
+              border: 0,
+              borderRadius: "5px",
+              backgroundColor: "#ccc",
+            }}
+            onClick={this.countReset}
+          >
+            RESET
+          </button>
+          <h1>{this.state.counter}</h1>
+        </div>
+        <form active="#" style={{ display: "flex", flexDirection: "column" }}>
+          <span>Yoshni kiriting! {this.state.age}</span>
+          <input
+            type="number"
+            min={1}
+            max={999}
+            className="form-control search-input mt-3"
+            onChange={this.changeHandler}
+          />
+        </form>
+      </>
     );
   }
 }
 
 function Counter() {
   const [isCount, setIsCount] = useState(0);
+
   return (
     <div style={{ width: "100%", display: "flex", gap: "10px" }}>
       <button
@@ -141,7 +159,12 @@ function Counter() {
 function TestBlock() {
   return (
     <div
-      style={{ marginTop: "30px", display: "flex", flexDirection: "column" }}
+      style={{
+        marginTop: "30px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}
     >
       <h1 style={{ fontSize: "19px" }}>
         TEST Block for new function components and class components or React
@@ -149,7 +172,6 @@ function TestBlock() {
       </h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
         <User firstName={"Behruz"} lastName={"Madaminov"} age={18} />
-
         <h1>Counter with function component</h1>
         <Counter />
         <h1>Counter with class component</h1>
